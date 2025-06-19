1a7f1f4415e9b297d0de1b653b59cad90c1bf242
Daniel Zadok <zadok.daniel@gmail.com>
2025-06-19 16:00:20 +0300

    class based views with mixins

--- 2025-06-19 - W171124MR/my_drf_project_1/blog/views.py
+++ 2025-06-19 - W171124MR/my_drf_project_1/blog/views.py
@@ -1,65 +1,36 @@
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
 
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.generics import GenericAPIView
 
-class PostView(APIView):
+
+class PostView(GenericAPIView, ListModelMixin, CreateModelMixin):
     name = "PostViews"
-    # throttle_classes = []
-    # permission_classes = []
-    # authentication_classes = []
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
     def get(self, request):
-        posts = Post.objects.all()
-        serialized = PostSerializer(posts, many=True)
-
-        return Response({'posts': serialized.data})
+        return self.list(request)
 
     def post(self, request):
-        post = PostSerializer(data=request.data)
-        post.is_valid(raise_exception=True)
-        post.save()
-        return Response(post.data, status=201)
+        return self.create(request)
 
 
-class PostViewByPK(APIView):
+class PostViewByPK(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     name = "PostByPKViews"
 
-    # throttle_classes = []
-    # permission_classes = []
-    # authentication_classes = []
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
     def get(self, request, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serialized = PostSerializer(post)
-        return Response({"post": serialized.data})
+        return self.retrieve(request, pk)
 
     def put(self, request, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serialized = PostSerializer(post, data=request.data)
-
-        if not serialized.is_valid():
-            return Response({"errors": serialized.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-        serialized.save()
-        return Response(serialized.data, status=status.HTTP_200_OK)
+        return self.update(request, pk)
 
     def delete(self, request, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.destroy(request, pk)
