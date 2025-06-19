a3a6d4b430277a4c493924c0a4c895bb2d740bd9
Daniel Zadok <zadok.daniel@gmail.com>
2025-06-19 15:52:22 +0300

    class based views

--- 2025-06-19 - W171124MR/my_drf_project_1/blog/urls.py
+++ 2025-06-19 - W171124MR/my_drf_project_1/blog/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",  views.posts),
-    path("<int:pk>/", views.post_by_pk),
 
-    path("posts/", views.posts),
-    path("posts/<int:pk>/", views.post_by_pk),
+    path('posts/', views.PostView.as_view()),
+    path('posts/<int:pk>/', views.PostViewByPK.as_view())
 ]
--- 2025-06-19 - W171124MR/my_drf_project_1/blog/views.py
+++ 2025-06-19 - W171124MR/my_drf_project_1/blog/views.py
@@ -2,25 +2,64 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def posts(request):
-    if request.method == 'GET':
+class PostView(APIView):
+    name = "PostViews"
+    # throttle_classes = []
+    # permission_classes = []
+    # authentication_classes = []
+
+    def get(self, request):
         posts = Post.objects.all()
         serialized = PostSerializer(posts, many=True)
 
         return Response({'posts': serialized.data})
-    elif request.method == 'POST':
+
+    def post(self, request):
         post = PostSerializer(data=request.data)
         post.is_valid(raise_exception=True)
         post.save()
         return Response(post.data, status=201)
 
 
-@api_view(['GET'])
-def post_by_pk(request, pk):
-    post = Post.objects.get(pk=pk)
-    serialized = PostSerializer(post)
+class PostViewByPK(APIView):
+    name = "PostByPKViews"
 
-    return Response(serialized.data)
+    # throttle_classes = []
+    # permission_classes = []
+    # authentication_classes = []
+
+    def get(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serialized = PostSerializer(post)
+        return Response({"post": serialized.data})
+
+    def put(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serialized = PostSerializer(post, data=request.data)
+
+        if not serialized.is_valid():
+            return Response({"errors": serialized.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        serialized.save()
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
